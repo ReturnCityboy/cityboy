4a2414c1d6479e55e66a96b64158dc33b1e4d0a3
DESKTOP-GKBRC31\cityboy <gooxgoo@naver.com>
2021-04-04 12:21:47 +0900

    에너미 추척 코루틴 에너미 애니메이션 ENUM

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,28 +2,161 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
+using Spine.Unity;
 
 public class Enemy : MonoBehaviour
 {
-    public Player player;//player
-    public float range = 10f;//range
+    private Transform transform;
+    private NavMeshAgent nvAgent;
+
+
+    public Player player;//공격대상
+    public float range = 10f;//추적용
+    public float attackrange = 5f;//공격용
+    public bool playerDir = true;//공격대상이 범위에들어왓는지
+    public bool isPlayer = true;//플레이어 생사 여부
+
     public int hp = 100;//enemy hp
     public int power = 100;//enemy power
     public float speed = 10f;//enemy speed
-    public bool playerDir = true;//player 방향
-    public float attackrange = 5f;//attackrange 
-    public bool isPlayer = true;//플레이어 생사 여부
-    public enum CurrentState { idle, move, attack, dead };//적 애니상태
 
-   void Start()
+    public enum CurrentState { IDLE, MOVE, ATTACK, DIE,RUN,JUMP, JUMPATTACK, SPECIAL,WALK };//적 애니상태
+    public CurrentState currentState = CurrentState.IDLE;
+    public SkeletonAnimation EnemyAni;
+
+    private void Awake()
     {
-        
+        transform = this.gameObject.GetComponent<Transform>();
+        player = GameObject.FindWithTag
+            ("Player").GetComponent <Player >();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        nvAgent.destination = player.transform.position;
+        StartCoroutine (CheckState());
+        StartCoroutine(CheckStateToAction());
     }
 
     void Update()
     {
-        
+       
+    }
+    IEnumerator CheckState()
+    {
+        while (isPlayer)
+        {
+            yield return new WaitForSeconds(0.1f);
+            float dis = Vector3.Distance(player.transform.position, transform.position);
+            if (dis <= attackrange)
+            {
+                currentState = CurrentState.ATTACK;
+            }
+            else if (dis <= range)
+            {
+                currentState = CurrentState.MOVE;
+            }
+            else
+            {
+                currentState = CurrentState.IDLE;
+
+            }
+        }
+       
+    }
+    IEnumerator CheckStateToAction()
+    {
+        while (isPlayer)
+        {
+            switch (currentState)
+            {
+                case CurrentState.IDLE:
+                   // nvAgent.Stop();
+                    break;
+
+                case CurrentState.MOVE:
+                    nvAgent.destination = player.transform.position;
+                   // nvAgent.Resume();
+                    break;
+
+                case CurrentState.ATTACK:
+                    break;
+
+            }
+
+            yield return null;
+        }
     }
 
-    
+    public void ChangeAni(CurrentState cur)
+    {
+        if (this.currentState == cur)
+        {
+            return;
+        }
+
+        if (EnemyAni == null)
+        {
+            Debug.Log("애니없음");
+            return;
+        }
+
+        switch (cur)
+        {
+
+            case CurrentState.IDLE:
+                EnemyAni.AnimationName = "idle";
+                EnemyAni.timeScale = 1f;
+                break;
+
+            case CurrentState.DIE:
+                EnemyAni.AnimationName = "die";
+                EnemyAni.timeScale = 1f;
+                break;
+
+            case CurrentState.ATTACK:
+                EnemyAni.AnimationName = "attack";
+                EnemyAni.timeScale = 1f;
+                break;
+
+            case CurrentState.RUN:
+                EnemyAni.AnimationName = "run";
+                EnemyAni.timeScale = 1f;
+                break;
+
+            /*case CurrentState.JUMP:
+                player.AnimationName = "jump";
+                player.timeScale = 1f;
+                break;
+
+            case CurrentState.JUMPATTACK:
+                player.AnimationName = "jumpattack";
+                player.timeScale = 1f;
+                break;
+
+            case CurrentState.DAMAGE:
+                player.AnimationName = "damage";
+                player.timeScale = 1f;
+                break;
+
+            case CurrentState.DOWN:
+                player.AnimationName = "down";
+                player.timeScale = 1f;
+                break;
+
+            case CurrentState.SPECIAL:
+                player.AnimationName = "special";
+                player.timeScale = 1f;
+                break;
+
+            case CurrentState.WALK:
+                player.AnimationName = "walk";
+                player.timeScale = 1f;
+                break;*/
+
+
+        }
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-
+    private static Player _instance;
+    public static Player Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Player();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
+    
+    public SkeletonAnimation player;
+    public enum ANI
+    {
+        IDLE,
+        ATTACK,
+        DAMAGE,
+        DOWN,
+        DIE,
+        RUN,
+        JUMP,
+        JUMPATTACK,
+        SPECIAL,
+        WALK
+    }
+    ANI ani = ANI.IDLE;
 
 
     void Start()
@@ -15,6 +48,75 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+    }
+    public void ChangeAni(ANI ani)
+    {
+        if (this.ani == ani)
+        {
+            return;
+        }
+
+        if (player == null)
+        {
+            Debug.Log("애니없음");
+            return;
+        }
+
+        switch (ani)
+        {
+
+            case ANI.IDLE:
+                player.AnimationName = "idle";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.DIE:
+                player.AnimationName = "die";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.ATTACK:
+                player.AnimationName = "attack";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.RUN:
+                player.AnimationName = "run";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.JUMP:
+                player.AnimationName = "jump";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.JUMPATTACK:
+                player.AnimationName = "jumpattack";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.DAMAGE:
+                player.AnimationName = "damage";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.DOWN:
+                player.AnimationName = "down";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.SPECIAL:
+                player.AnimationName = "special";
+                player.timeScale = 1f;
+                break;
+
+            case ANI.WALK:
+                player.AnimationName = "walk";
+                player.timeScale = 1f;
+                break;
+
+
+        }
     }
 
     public void Attack()
