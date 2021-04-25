7f86f0109fceadd87136f58cb72d8a7fc0c8b00b
DESKTOP-GKBRC31\cityboy <gooxgoo@naver.com>
2021-04-25 13:27:11 +0900

    for문 배열사용 테스트

---
+++ Assets/Scripts/TesePractice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TesePractice : MonoBehaviour
+{
+    public Button buttonA;
+    public string a = "a";
+    public string b = "b";
+    public string c = "c";
+
+    
+    public Text text;
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+    }
+   
+
+
+
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    public Button button;
+    /*public Button button;
     public int length = 10;
-    public int a = 1;
-  
-    // Start is called before the first frame update
+    public int a = 1;*/
+
+    public Button button;//버튼사용하겠다
+    public string[] logs;//로그라는 이름의 배열을 사용하겠다
+
+
+    private void Awake()
+    {
+        logs = new string [4] { "안녕", "뭐해", "어디가", "잘가" };
+    }
     void Start()
     {
         
@@ -21,15 +28,29 @@ public class Test : MonoBehaviour
         
     }
 
-    public void Click()
+    /* public void Click()
+     {
+         for (int i = 0; i < length; i++)
+         {
+
+             a += 1;
+             Debug.Log(a);
+         }
+
+     }*/
+
+    
+
+    public void LogsButton()
     {
-        for (int i = 0; i < length; i++)
+
+
+        for (int i = 0; i < logs.Length; i++)
         {
-            
-            a += 1;
-            Debug.Log(a);
+            Debug.Log(logs[i]);
         }
-        
     }
+    
+
 
 }
